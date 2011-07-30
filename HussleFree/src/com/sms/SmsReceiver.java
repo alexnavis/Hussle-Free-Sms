1d464220499af14ccbbd500f0cdf25a99eceef41
Ashok <ashok@Ashokkumar-Ramdosss-MacBook-Pro.local>
2011-07-30 11:43:00 +0530

    Ashok - Intial delete SMS

--- HussleFree/src/com/sms/SmsReceiver.java
+++ HussleFree/src/com/sms/SmsReceiver.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources.Theme;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,8 @@ public class SmsReceiver extends BroadcastReceiver {
 
 	static final String ACTION =
 			            "android.provider.Telephony.SMS_RECEIVED";
+	private Thread thread;    
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 	
@@ -32,8 +35,9 @@ public class SmsReceiver extends BroadcastReceiver {
 		}
 
 		// show first message
-		Log.d(null,"Received SMS: " + smsMessage[0].getMessageBody() );
-		Toast toast = Toast.makeText(context, "Received SMS: " + smsMessage[0].getMessageBody(), Toast.LENGTH_LONG);
+		String messageBody = smsMessage[0].getMessageBody();
+		Log.d(null,"Received SMS: " + messageBody );
+		Toast toast = Toast.makeText(context, "Received SMS: " + messageBody, Toast.LENGTH_LONG);
 		toast.show();
 			
 		Uri uriSms = Uri.parse("content://sms/inbox");
@@ -43,7 +47,29 @@ public class SmsReceiver extends BroadcastReceiver {
 			Log.d(null, "M"  + m);
 		}
 		
-		abortBroadcast();
+try {
+	wait(3000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+		
+		Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+		
+		while (cursor.moveToNext()) {
+
+			 long thread_id = cursor.getLong(1);
+			 final String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+			 Log.d(null, " thread " + thread_id);
+
+			 if(messageBody.equals(body)){
+				 Uri thread = Uri.parse("content://sms/conversations/" + thread_id);
+				 context.getContentResolver().delete(thread, null, null);
+				 Log.d(null, "Delete msg " + body + " thred " + thread_id);
+			 }
+	    }
+
+//		abortBroadcast();
 		Log.d(null, "Intent ReceivedVVVVVVVVVVVVVVVVVVVv end"  + intent.getAction());
 	}
 	
